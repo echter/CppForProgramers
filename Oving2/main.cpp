38c27a6088a7ac5a1ae3ffc7995a1622cdfa69cb
echter <chrech.birr@gmail.com>
2017-11-01 16:48:53 +0000

    first part of exercise 2 done

--- Oving2/main.cpp
+++ Oving2/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  
+  bool oppgave1 = true;
+  bool oppgave4 = false;
+  
+  if (oppgave1){
+    // Exercise 1a
+    int i = 3;
+    int j = 5;
+    int *p = &i;
+    int *q = &j;
+    
+    cout << "value: " << i << " Adress: " << &i << endl;
+    cout << "value: " << j << " Adress: " << &j << endl;
+    cout << "value: " << p << " Adress: " << &p << endl;
+    cout << "value: " << q << " Adress: " << &q << endl;
+    
+    // Exercise 1b
+    *p = 7;
+    *q += 4;
+    *q = *p + 1;
+    p = q;
+    cout << *p << " " << *q << endl;
+  }
+  
+  else if (oppgave4){
+    
+  }
 }
