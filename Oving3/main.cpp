75aff7ed65fc1665050f16da5c68478354cbf8ea
echter <chrech.birr@gmail.com>
2017-11-03 13:07:24 +0000

    Ex1 Ov3 done

--- Oving3/main.cpp
+++ Oving3/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+// Oppgave 1 --------------------------------------------------------------------------------------------
+const double pi = 3.141592;
+class Sirkel {
+public:
+  Sirkel(double startRadius);
+  int finnAreal() const;
+  double finnOmkrets() const;
+private:
+  double radius;
+};
+// ==> Implementasjon av klasse
+
+Sirkel::Sirkel(double startRadius) : radius(startRadius) {}
+
+int Sirkel::finnAreal() const{
+  return pi * radius * radius;
+}
+
+double Sirkel::finnOmkrets() const {
+  double omkrets = 2.0 * pi * radius;
+  return omkrets;
+}
+// End Oppgave 1 --------------------------------------------------------------------------------------
+
 int main() {
   std::cout << "Hello World!\n";
 }
