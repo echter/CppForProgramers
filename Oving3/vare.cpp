cdb5a582aa657d6d0b1783eb13044a090f59ff44
echter <chrech.birr@gmail.com>
2017-11-18 13:33:52 +0000

    soon done with ex3

--- Oving3/vare.cpp
+++ Oving3/vare.cpp
@@ -2,19 +2,45 @@
 
 using namespace std;
 
+const double moms = 15.0;
 class Vare{
 public:
+  Vare(string vareNavn, int vareNr, double pris);
   string finnNavn();
   int finnVareNr();
   double finnPrisPerEnhet();
   void settPris(double nyPris);
-  double finnPrisUtenMoms();
-  double finnPrisMedMoms();
+  double finnPrisUtenMoms(double antallVare);
+  double finnPrisMedMoms(double antallVare);
   
 private:
+  string vareNavn;
   int vareNr;
   double pris;
-  string vareNavn;
 };
 
-Vare::Vare(string vareNavn, int vareNr, double pris){};
+Vare::Vare(string vareNavn, int vareNr, double pris) : vareNavn(vareNavn), vareNr(vareNr), pris(pris){};
+
+string Vare::finnNavn(){
+  return vareNavn;
+}
+
+int Vare::finnVareNr(){
+  return vareNr;
+}
+
+double Vare::finnPrisPerEnhet(){
+  return pris;
+}
+
+void Vare::settPris(double pris){
+  Vare::pris = pris;
+}
+
+double Vare::finnPrisUtenMoms(double antallVare){
+  return antallVare;
+}
+
+double Vare::finnPrisMedMoms(double antallVare){
+  return antallVare * moms;
+}
