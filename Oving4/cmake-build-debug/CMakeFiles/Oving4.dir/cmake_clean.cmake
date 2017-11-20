bb02f4f954cf7ca0be9b33213ad4706e5e5c50d5
Christian Echtermeyer <chrech.birr@gmail.com>
2017-11-20 14:22:20 +0100

    attempting to fix gitignore

---
+++ Oving4/cmake-build-debug/CMakeFiles/Oving4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oving4.dir/main.cpp.obj"
+  "Oving4.pdb"
+  "Oving4.exe"
+  "Oving4.exe.manifest"
+  "libOving4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oving4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
